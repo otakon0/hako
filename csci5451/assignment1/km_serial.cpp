19585f76e8d629298515eb2119032ed8d4cea04d
otakon0 <riceman01232@icloud.com>
2019-02-22 21:28:32 -0600

    thread works

--- csci5451/assignment1/km_serial.cpp
+++ csci5451/assignment1/km_serial.cpp
@@ -11,6 +11,8 @@
 #include <sstream>
 #include <string>
 
+
+#define MAX_LOOP 20
 /* Assume that this runs on LINUX */
 static inline double monotonic_seconds(){
 	struct timespec ts;
@@ -21,6 +23,13 @@ static void print_time(const double seconds){
 	printf("k-means clustering time: %0.04fs\n",seconds);
 }
 
+static inline double getDistance(double *point,double *centroid,int dim){
+	double distance = 0.0;
+	for(int i=0;i<dim;++i){
+	    distance += (point[i]-centroid[i])*(point[i]-centroid[i]);
+	}
+	return distance;
+}
 
 /* large.txt 509256 500 */
 int main(int argc,char *argv[]){
@@ -36,28 +45,31 @@ int main(int argc,char *argv[]){
 	    return 0;
 	}
 
-	/* Assume that the first line reads Number of data and The dimention */
-	int ncluster,dimension;
+	int numclusters;
+	int numdimensions;
+	int numcentroids;
+	int numthreads;
 
+	/* read the data points from input files */
 	{
+	    // read the first line
 	    std::string line;
 	    getline(file,line);
 	    std::istringstream stream(line);
 	    std::string token;
-	    stream >> ncluster >> dimension;
+	    stream >> numclusters >> numdimensions;
 	}
 
 	printf("The number of cluster    : %d\n"
 	       "The number #of dimension : %d\n",
-		ncluster,dimension);
+		numclusters,numdimensions);
 
 	double **data_list;
-	data_list = new double*[ncluster];
-	for(int i=0;i<ncluster;++i){
-	    data_list[i] = new double[dimension];
+	data_list = new double*[numclusters];
+	for(int i=0;i<numclusters;++i){
+	    data_list[i] = new double[numdimensions];
 	}
 
-	/* read data from the inputfile */
 	{
 	    std::string line;
 	    int i = 0,j = 0;
@@ -67,26 +79,93 @@ int main(int argc,char *argv[]){
 	        while(getline(stream,token,' ')){
 		    data_list[i][j] = std::stod(token);
 		    j++;
-		    if(j>=dimension) break;
+		    if(j>=numdimensions) break;
 		}
-	    
+
 	    i++;
 	    j = 0;
-	    if(i>=ncluster) break;
-	    }	
+	    if(i>=numclusters) break;
+	    }
 	}
-	file.close();
-	const double start_time = monotonic_seconds();
 
+	file.close();
+
+
+        /* choose the first some data points as centroids */
+        numcentroids = std::stoi(argv[2]);
+        double **centroids;
+        centroids = new double*[numcentroids];
+        for(int i=0;i<numcentroids;++i){
+            centroids[i] = new double[numdimensions];
+        }
+        for(int i=0;i<numcentroids;++i){
+            for(int j=0;j<numdimensions;++j){
+                centroids[i][j] = data_list[i][j];
+            }
+        }
+
+
+
+	const double start_time = monotonic_seconds();
 	/* k-means */
+	int *position = new int[numclusters];
+	int *counter  = new int[numcentroids];
+	double **centroidsum  = new double*[numcentroids];
+	for(int i=0;i<numcentroids;++i){
+	    centroidsum[i] = new double[numdimensions];
+	}
+	for(int i=0;i<MAX_LOOP;++i){
+
+	    for(int j=0;j<numcentroids;++j){
+		counter[j] = 0;
+		for(int k=0;k<numdimensions;++k)
+		    centroidsum[j][k] = 0.0;
+	    }
+
+	    for(int j=0;j<numclusters;++j){
+		double mindistance = getDistance(data_list[j],centroids[0],numdimensions);
+		int minposition = 0;
+		for(int k=1;k<numcentroids;++k){
+		    double tmp = getDistance(data_list[j],centroids[k],numdimensions);
+		    if(tmp < mindistance){
+			mindistance = tmp;
+			minposition = k;
+		    }
+		}
+	        for(int k=0;k<numdimensions;++k){
+		    centroidsum[minposition][k] += data_list[j][k];
+		}
+		counter[minposition]     += 1;
+		position[j] = minposition;
+	    }
+
+	    /* update centroids position */
+	    for(int j=0;j<numcentroids;++j){
+		int cnt = counter[j];
+		if(cnt == 0){
+		    printf("Fatal Error : A centroid does not have any close data\n");
+		    exit(1);
+		}
+	        for(int k=0;k<numdimensions;++k){
+		    centroids[j][k] = centroidsum[j][k] / cnt;
+		}
+	    }
+	}
 
 	const double end_time   = monotonic_seconds();
 	print_time(end_time - start_time);
 
-	for(int i=0;i<ncluster;++i){
+
+	for(int i=0;i<numclusters;++i){
 	    delete [] data_list[i];
 	}
-
+	for(int i=0;i<numcentroids;++i){
+	    delete [] centroidsum[i];
+	}
 	delete [] data_list;
+	delete [] centroids;
+	delete [] centroidsum;
+	delete [] counter;
+	delete [] position;
 	return 0;
 }
